6c92525b3c845e211f138438bfb6930ecdc69ebc
JuandiGuti <juandiegoguti2302@gmail.com>
2023-08-30 07:43:27 -0600

    Se creo el proyecto MVC y se coloco el Controller para la carga de archivo

---
+++ LAB1-EDD2/Controllers/FileLoaderController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAB1_EDD2.Controllers
+{
+    [Route("[controller]")]
+    public class FileLoaderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("load-interface")]
+        public IActionResult load(IFormFile file)
+        {
+			if (file != null)
+			{
+				try
+				{
+					string path = Path.Combine(Path.GetTempPath(), file.Name);
+					using (var stream = new FileStream(path, FileMode.Create))
+					{
+						file.CopyTo(stream);
+					}
+
+					string fullFile = System.IO.File.ReadAllText(path);
+
+					foreach (string line in fullFile.Split('\n'))
+					{
+						if (!string.IsNullOrEmpty(line))
+						{
+							string[] actualRow = line.Split(',');
+							//Logica de ingreso de datos
+						}
+					}
+				}
+				catch (Exception e)
+				{
+					ViewBag.Error = "Error al leer el archivo" + e.Message;
+				}
+			}
+			else
+			{
+				ViewBag.Error = "No se ha ingresado la ruta de archivo";
+			}
+			return View(/*Mandar lista con elementos*/);
+		}
+	}
+}
---
+++ LAB1-EDD2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LAB1_EDD2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
