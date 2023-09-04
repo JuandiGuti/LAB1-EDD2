7f97c5c11458631789327baab86ae09797f9f961
JuandiGuti <juandiegoguti2302@gmail.com>
2023-09-03 19:03:39 -0600

    Metodo de insersion con comprobaciones

--- LAB1-EDD2/Controllers/HomeController.cs
+++ LAB1-EDD2/Controllers/HomeController.cs
@@ -1,11 +1,17 @@
 ﻿using LAB1_EDD2.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace LAB1_EDD2.Controllers
 {
     public class HomeController : Controller
     {
+        public static List<string> instruccionsList = new List<string>();
+        public static List<person> jsonListString = new List<person>();
+
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -17,11 +23,93 @@ namespace LAB1_EDD2.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public bool insert(person personJson, bool canBeInserted)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+			if (jsonListString.Count() > 0)
+			{
+				foreach (person foreachPerson in jsonListString)
+				{
+					if (foreachPerson.dpi == personJson.dpi)
+					{
+						canBeInserted = false;
+					}
+				}
+				if (canBeInserted)
+				{
+					jsonListString.Add(personJson);
+
+				}
+			}
+			else if (canBeInserted)
+			{
+				jsonListString.Add(personJson);
+			}
+			else
+			{
+				ViewBag.Error = "Error al ingresar el Json";
+			}
+			return true;
+        }
+
+        [HttpPost("load-interface")]
+        public IActionResult load(IFormFile file)
+        {
+            if (file != null)
+            {
+                try
+                {
+                    string path = Path.Combine(Path.GetTempPath(), file.Name);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    string fullFile = System.IO.File.ReadAllText(path);
+
+                    foreach (string line in fullFile.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            string[] actualRow = line.Split(';');
+                            bool canBeInserted = true;
+                            person personJson = JsonSerializer.Deserialize<person>(actualRow[1]);
+                            if (actualRow[0] == "INSERT")
+                            {
+                                insert(personJson, canBeInserted);
+							}
+                            else if (actualRow[0] == "PATCH")
+                            {
+                                foreach (person foreachPerson in jsonListString)
+                                {
+                                    if (foreachPerson == personJson)
+                                    {
+                                        foreachPerson.datebirth = personJson.datebirth;
+										foreachPerson.address = personJson.address;
+									}
+                                }
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Error = "Error al leer el archivo" + e.Message;
+                }
+            }
+            else
+            {
+                ViewBag.Error = "No se ha ingresado la ruta de archivo";
+            }
+            return View(/*Mandar lista con elementos*/);
+        }
+        [Route("load-interface")]
+        public IActionResult load()
+        {
+            return View();
         }
     }
 }
---
+++ LAB1-EDD2/Models/person.cs
@@ -0,0 +1,10 @@
+﻿namespace LAB1_EDD2.Models
+{
+	public class person
+	{
+		public string name { get; set; }
+		public string dpi { get; set; }
+		public string datebirth { get; set; }
+		public string address { get; set; }
+	}
+}
